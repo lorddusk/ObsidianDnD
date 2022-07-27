b487e68bae018039592eb78145b70aa8a8cc4dfc
Tim Langen <tim.langen@driessen.nl>
2022-07-27 13:53:03 +0200

    vault backup: 2022-07-27 13:53:03

--- Templates/scripts/session_number.py
+++ Templates/scripts/session_number.py
@@ -5,7 +5,10 @@ import traceback
 def main():
     if sys.stdout.encoding != 'utf-8':
         sys.stdout.reconfigure(encoding='utf-8')
-    
+    file = open("session_number.txt", "W+")
+    number = file.read()
+    newNumber = f"{number + 1}"
+    file
 
 
 if __name__ == '__main__':
