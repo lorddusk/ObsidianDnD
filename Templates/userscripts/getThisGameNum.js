0771a19088c9a5fe477b8787ab219158ed4eb32e
LordDusk <lang2619@gmail.com>
2022-10-03 15:46:36 +0200

    vault backup: 2022-10-03 15:46:36

--- Templates/userscripts/getThisGameNum.js
+++ Templates/userscripts/getThisGameNum.js
@@ -1,17 +1,17 @@
 function getThisGameNum(tp){
     let folderName = tp.file.folder(false);
-    app.plugins.plugins.dataview.api.pages(`"Red Hand of Doom/${folderName}"`).forEach(page => console.log(page.sessionNum));
-    let numOfGames = app.plugins.plugins.dataview.api
-        .pages(`"Red Hand Of Doom/${folderName}"`)
-        .where(page => {
-            if(page.type === 'session'){
-                console.log("Page found");
-                if(page.campaign === "Red Hand of Doom"){
-                    console.log('SessionNum: ', page.sessionNum);
-                    return true;
-                }
+    let numOfGames = 0;
+    app.plugins.plugins.dataview.api.pages(`"Red Hand of Doom/${folderName}"`)
+    .forEach(page => {
+        if(page.type === 'session'){
+            console.log("Page found");
+            if(page.campaign === "Red Hand of Doom"){
+                console.log('SessionNum: ', page.sessionNum);
+                numOfGames += 1;
             }
-        }).length
+        }
+    });
+    
     numOfGames = JSON.stringify(numOfGames+1);
     while(numOfGames.length < 3){
         numOfGames = "0"+numOfGames;
